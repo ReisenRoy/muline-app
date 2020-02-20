702d2b2c170ac3737ab3d0920ddffdb7fcff9e1d
Kristina Andreeva <tabbyk@ya.ru>
2020-02-20 21:57:18 +0300

    remove button module

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,6 @@
   import ShowItem from "./ShowItem.svelte";
 	import Search from "./Modules/Search.svelte";
 	import Navigation from "./Modules/Navigation.svelte";
-  import Button from "./UI/Button.svelte";
 	import { mulines } from "./Modules/muline-store.js";
 
 	let mulineList;
@@ -11,33 +10,8 @@
 		mulineList = value;
 	});
 
-  let isfullTable = false;
-
   function showItem() {
 		console.log("show");
-  }
-
-  function toggleTable() {
-    const elementsFT = document.querySelectorAll(".elementsFT");
-    for(let i = 0; i < elementsFT.length; i++){
-      elementsFT[i].classList.toggle("invis");
-    }
-  }
-
-  function inStock() {
-    const inStock = document.querySelectorAll(".inStock");
-    const stockNum = document.querySelector(".stockNum");
-    const tr = document.querySelectorAll("tr");
-    let count = 0;
-    for(let i = 0; i < inStock.length; i++ ){
-      if (Number(inStock[i].innerHTML) <= 0) {
-        tr[i].classList.toggle("invisRow");
-      }
-      count += Number(inStock[i].innerHTML);
-      stockNum.innerHTML = count + " шт";
-    }
-
-    isfullTable = true;
   }
 </script>
 
@@ -63,7 +37,7 @@
             { muline.stock }
           </td>
           <td class="td_button">
-            <Button on:click={showItem}>{ muline.DMC }</Button>
+            { muline.DMC }
           </td>
           <td>
             { muline.Kirova }
