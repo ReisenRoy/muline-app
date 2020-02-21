ab18d2770147022b2821641cae6312d73a7616b0
Kristina Andreeva <tabbyk@ya.ru>
2020-02-21 18:22:38 +0300

    remove ShowItem

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,15 @@
 <script>
 	import EditItem from "./EditItem.svelte";
-  import ShowItem from "./Modules/ShowItem.svelte";
+  import Item from "./Modules/Item.svelte";
 	import Search from "./Modules/Search.svelte";
-	import Navigation from "./Modules/Navigation.svelte";
-	import { mulines } from "./Modules/muline-store.js";
+  import Navigation from "./Modules/Navigation.svelte";
+  import { mulines } from "./Modules/muline-store.js";
 
-	let currentMuline = "";
 	let mulineList;
 	const unsubscribe = mulines.subscribe((value) => {
 		mulineList = value;
 	});
-
-  function showMulineInfo(item) {
-    currentMuline = item;
-  }
 </script>
-
-<style>
-	tr {
-		cursor: pointer;
-	}
-</style>
 
 <h1>Список</h1>
 
@@ -39,29 +28,8 @@
     </thead>
     <tbody>
       {#each mulineList as muline, id (muline.DMC)}
-        <tr on:click={showMulineInfo(muline)}>
-          <td class="inStock">
-            { muline.stock }
-          </td>
-          <td class="td_button">
-            { muline.DMC }
-          </td>
-          <td>
-            { muline.Kirova }
-          </td>
-          <td class="elementsFT">
-            { muline.Anchor }
-          </td>
-          <td class="elementsFT">
-            { muline.Madeira }
-          </td>
-          <td style= "background: #{ muline.color }" class="color" title="{ muline.color_en }"></td>
-        </tr>
+        <Item {muline} />
       {/each}
     </tbody>
   </table>
 </div>
-
-{#if currentMuline}
-	<ShowItem {currentMuline} />
-{/if}
---
+++ src/Modules/Item.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let muline;
+
+</script>
+
+<style>
+	tr {
+		cursor: pointer;
+	}
+</style>
+
+<tr>
+  <td class="inStock">
+    { muline.stock }
+  </td>
+  <td class="td_button">
+    { muline.DMC }
+  </td>
+  <td>
+    { muline.Kirova }
+  </td>
+  <td class="elementsFT">
+    { muline.Anchor }
+  </td>
+  <td class="elementsFT">
+    { muline.Madeira }
+  </td>
+  <td style= "background: #{ muline.color }" class="color" title="{ muline.color_en }"></td>
+</tr>
